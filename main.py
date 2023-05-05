ace3ba5bccf81d31b41ccad4593374df7e95a5a3
Akshat Punia <92669600+akshatpunia26@users.noreply.github.com>
2023-05-06 04:59:19 +0530

    new auth

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ client_id = 'dc8611201d2a4d68ac59e3623d309096'
 client_secret = '470122036a274706a4f705ab88867fed'
 scope = 'user-library-read'
 redirect_uri = 'https://modern-love-spotify.streamlit.app/'
+
+st.title('Love in Pop Culture')
+st.subheader('Modern Love: Writing and Deconstructing the Idea - CCC338 Creative Project')
+st.markdown('[Akshat Punia](https://akshatpunia.com/)')
+
 auth_manager = SpotifyOAuth(client_id=client_id, client_secret=client_secret, scope=scope, redirect_uri=redirect_uri)
-sp = spotipy.Spotify(auth_manager=auth_manager)
+
+try:
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+    st.write('Authentication successful!')
+except:
+    st.write('Authentication failed. Please try again later.')
 
 decades = {'1920s': 'https://open.spotify.com/playlist/7HWmbcjkVTfQdLKk2NWmtZ?si=6b870f33efe24872' ,
            '1930s': 'https://open.spotify.com/playlist/2webFRRyZuTUdw9KrMKT63?si=08ee3c375cba4319' ,
            '1940s': 'https://open.spotify.com/playlist/1KTSHAsbCgSnZLMdXp52Ln?si=93366e637d9349ca' ,
            '1950s': 'https://open.spotify.com/playlist/431BefAztO1PGCR4yd6PXL?si=231a737083dc4906'}
-
-st.title('Love in Pop Culture')
-st.subheader('Modern Love: Writing and Deconstructing the Idea - CCC338 Creative Project')
-st.markdown('[Akshat Punia](https://akshatpunia.com/)')
 
 decade = st.slider('Select a decade', 1920, 2010, 1980, 10)
 
@@ -27,8 +33,14 @@ if st.button('Generate Playlist'):
         playlist_uri = decades[decade_str]
         playlist = sp.playlist_tracks(playlist_uri)
         tracks = playlist['items']
-        tracks_uris = [track['track']['uri'] for track in tracks]
-        sp.start_playback(uris=tracks_uris)
-        st.write('Enjoy your custom playlist!')
+        if not tracks:
+            st.write('Sorry, the selected playlist is empty.')
+        else:
+            tracks_uris = [track['track']['uri'] for track in tracks]
+            try:
+                sp.start_playback(uris=tracks_uris)
+                st.write('Enjoy your custom playlist!')
+            except:
+                st.write('Unable to play the selected playlist. Please try again later.')
     else:
         st.write('Sorry, no playlist found for this decade.')
