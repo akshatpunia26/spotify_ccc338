8bcd8ddfe77b3abccd9ae39d65b0875818e67c26
Akshat Punia <92669600+akshatpunia26@users.noreply.github.com>
2023-05-06 09:07:59 +0530

    man this shit sucks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+# Define the available decades
 DECADES = {
     "1920s": "1920-1929",
     "1930s": "1930-1939",
@@ -15,6 +16,7 @@ DECADES = {
     "2010s": "2010-2019",
 }
 
+# Set up authentication manager
 auth_manager = SpotifyOAuth(
     client_id='dc8611201d2a4d68ac59e3623d309096',
     client_secret='470122036a274706a4f705ab88867fed',
@@ -23,10 +25,8 @@ auth_manager = SpotifyOAuth(
     cache_path='.spotipyoauthcache'
 )
 
-playlist_name = None
-description = None
+# Get access token
 access_token = None
-
 try:
     token_info = auth_manager.get_cached_token()
     if token_info:
@@ -35,12 +35,6 @@ except:
     pass
 
 if not access_token:
-    playlist_name = st.text_input("Enter a name for the playlist:")
-    description = st.text_input("Enter a description for the playlist:")
-
-    decade1 = st.selectbox("Select the first decade:", options=list(DECADES.keys()))
-    decade2 = st.selectbox("Select the second decade:", options=list(DECADES.keys()))
-
     auth_url = auth_manager.get_authorize_url()
     st.write('Please visit this URL to authorize the application:', auth_url)
     response = st.experimental_get_query_params()
@@ -51,21 +45,28 @@ if not access_token:
     else:
         st.stop()
 
-if playlist_name and description and access_token:
-    spotify = spotipy.Spotify(auth=access_token)
-    user_dict = spotify.current_user()
+# Use access token to get current user
+spotify = spotipy.Spotify(auth=access_token)
+user_dict = spotify.current_user()
 
-    playlist = spotify.user_playlist_create(user_dict['id'], name=playlist_name, public=False, description=description)
+# Create the playlist
+playlist_name = st.text_input("Enter a name for the playlist:")
+description = st.text_input("Enter a description for the playlist:")
+playlist = spotify.user_playlist_create(user_dict['id'], name=playlist_name, public=False, description=description)
 
-    track_uris = []
+# Get the two decades from the user
+decade1 = st.selectbox("Select the first decade:", options=list(DECADES.keys()))
+decade2 = st.selectbox("Select the second decade:", options=list(DECADES.keys()))
 
-    for decade in [decade1, decade2]:
-        st.write(f"Searching for {decade} songs...")
-        results = spotify.search(q=f"year:{DECADES[decade]}", type="track", limit=10)
-        for track in results["tracks"]["items"]:
-            track_uris.append(track["uri"])
+# Initialize an empty list to hold the track URIs
+track_uris = []
 
-    spotify.playlist_add_items(playlist["id"], track_uris)
-
-    st.write(f"Playlist '{playlist_name}' created with {len(track_uris)} songs.")
-    st.write(f"Click here to view the playlist on Spotify: {playlist['external_urls']['spotify']}")
+# Search for tracks from the selected decades and add them to the list
+for decade in [decade1, decade2]:
+    st.write(f"Searching for {decade} songs...")
+    results = spotify.search(q=f"year:{DECADES[decade]}", type="track", limit=10)
+    for track in results["tracks"]["items"]:
+        track_uris.append(track["uri"])
+# Add the tracks to the playlist
+spotify.playlist_add_items(playlist["id"], track_uris)
+st.write(f"Playlist '{playlist_name}' created with {len(track_uris)} songs.")
