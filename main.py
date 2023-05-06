7715a305e4f82131293af9c34478916d95ca9040
Akshat Punia <92669600+akshatpunia26@users.noreply.github.com>
2023-05-06 08:32:42 +0530

    man this shit sucks

--- main.py
+++ main.py
@@ -2,51 +2,63 @@ import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# Spotify API credentials
-CLIENT_ID = "dc8611201d2a4d68ac59e3623d309096"
-CLIENT_SECRET = "470122036a274706a4f705ab88867fed"
-REDIRECT_URI = "https://modern-love-spotify.streamlit.app/"
+DECADES = {
+    "1920s": "1920-1929",
+    "1930s": "1930-1939",
+    "1940s": "1940-1949",
+    "1950s": "1950-1959",
+    "1960s": "1960-1969",
+    "1970s": "1970-1979",
+    "1980s": "1980-1989",
+    "1990s": "1990-1999",
+    "2000s": "2000-2009",
+    "2010s": "2010-2019",
+}
 
-# Spotify scope
-SCOPE = "playlist-modify-private"
+auth_manager = SpotifyOAuth(
+    client_id='dc8611201d2a4d68ac59e3623d309096',
+    client_secret='470122036a274706a4f705ab88867fed',
+    redirect_uri='https://modern-love-spotify.streamlit.app/',
+    scope='playlist-modify-private,playlist-modify-public',
+    cache_path='.spotipyoauthcache'
+)
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI))
+access_token = None
+try:
+    token_info = auth_manager.get_cached_token()
+    if token_info:
+        access_token = token_info['access_token']
+except:
+    pass
 
-def get_track_ids_from_decades(decades):
-    track_ids = []
-    for decade in decades:
-        year_range = f"{decade}-01-01:{decade+9}-12-31"
-        results = sp.search(q=year_range, type='track', limit=50)
-        for track in results['tracks']['items']:
-            track_ids.append(track['id'])
-    return track_ids
+if not access_token:
+    auth_url = auth_manager.get_authorize_url()
+    st.write('Please visit this URL to authorize the application:', auth_url)
+    response = st.experimental_get_query_params()
+    if 'code' in response:
+        auth_code = response['code'][0]
+        token_info = auth_manager.get_access_token(auth_code)
+        access_token = token_info['access_token']
+    else:
+        st.stop()
 
-def create_playlist(name, description, track_ids):
-    user_dict = sp.current_user()
-    playlist = sp.user_playlist_create(user_dict['id'], name=name, public=False, description=description)
-    sp.playlist_add_items(playlist['id'], track_ids)
-    st.success("Playlist created successfully!")
+spotify = spotipy.Spotify(auth=access_token)
+user_dict = spotify.current_user()
 
-def app():
-    st.header("Create a Spotify Playlist")
+playlist_name = st.text_input("Enter a name for the playlist:")
+description = st.text_input("Enter a description for the playlist:")
+playlist = spotify.user_playlist_create(user_dict['id'], name=playlist_name, public=False, description=description)
 
-    # Input playlist name
-    playlist_name = st.text_input("Enter playlist name")
+decade1 = st.selectbox("Select the first decade:", options=list(DECADES.keys()))
+decade2 = st.selectbox("Select the second decade:", options=list(DECADES.keys()))
 
-    # Input playlist description
-    description = st.text_input("Enter playlist description")
+track_uris = []
+for decade in [decade1, decade2]:
+    st.write(f"Searching for {decade} songs...")
+    results = spotify.search(q=f"year:{DECADES[decade]}", type="track", limit=10)
+    for track in results["tracks"]["items"]:
+        track_uris.append(track["uri"])
+        st.write(f"{track['name']} by {track['artists'][0]['name']} ({decade}) - {track['external_urls']['spotify']}")
 
-    # Input decades
-    decades = st.multiselect("Select decades", [i for i in range(1920, 2020, 10)])
-
-    # Display playlist name and description
-    if playlist_name and description and decades:
-        track_ids = get_track_ids_from_decades(decades)
-        create_playlist(playlist_name, description, track_ids)
-    elif decades:
-        st.warning("Please enter a playlist name and description")
-    elif playlist_name or description:
-        st.warning("Please select at least one decade")
-
-if __name__ == "__main__":
-    app()
+spotify.playlist_add_items(playlist["id"], track_uris)
+st.write(f"Playlist '{playlist_name}' created with {len(track_uris)} songs.")
