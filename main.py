21c3efee9fbcb8df8f33c86a64d76f4780dfdbde
Akshat Punia <92669600+akshatpunia26@users.noreply.github.com>
2023-05-06 14:18:36 +0530

    self affirmations sustain me at this point

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 playlist_data = pd.read_csv('playlist_data.csv')
 
 st.title('Decades Playlist Generator')
-st.markdown("""Select two decades from 1920 to 2010, and we'll generate a playlist of the top 20 songs from each decade!""")
+st.markdown("""Select two decades from 1920 to 2010, and we'll generate a playlist of all songs from each decade!""")
 
 decades_dict = {'1920s': 1920,
                 '1930s': 1930,
@@ -21,14 +21,13 @@ decades = list(decades_dict.keys())
 decade1 = st.selectbox('Select the first decade:', decades)
 decade2 = st.selectbox('Select the second decade:', decades)
 
-start_year1 = str(decades_dict[decade1])
-end_year1 = str(decades_dict[decade1] + 10)
-start_year2 = str(decades_dict[decade2])
-end_year2 = str(decades_dict[decade2] + 10)
-
 if st.button('Generate Playlist'):
-    playlist1 = playlist_data[(playlist_data['decade'] == decade1) & (playlist_data['playlist_uri'].str[:4] >= start_year1) & (playlist_data['playlist_uri'].str[:4] < end_year1)].head(20)
-    playlist2 = playlist_data[(playlist_data['decade'] == decade2) & (playlist_data['playlist_uri'].str[:4] >= start_year2) & (playlist_data['playlist_uri'].str[:4] < end_year2)].head(20)
-    playlist = pd.concat([playlist1, playlist2]).reset_index(drop=True)
+    playlist1 = playlist_data[playlist_data['decade'] == decade1]
+    playlist2 = playlist_data[playlist_data['decade'] == decade2]
     st.markdown(f"## Your {decade1} + {decade2} Playlist")
-    st.write(playlist[['artist', 'name']])
+    st.write(f"### {decade1}")
+    for index, row in playlist1.iterrows():
+        st.write(f"[{row['name']} - {row['artist']}]({row['playlist_uri']})")
+    st.write(f"### {decade2}")
+    for index, row in playlist2.iterrows():
+        st.write(f"[{row['name']} - {row['artist']}]({row['playlist_uri']})")
