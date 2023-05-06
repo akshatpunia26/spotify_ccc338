b1177fe4042f2e038af05fd8c490eb59a6c17935
Akshat Punia <92669600+akshatpunia26@users.noreply.github.com>
2023-05-06 14:26:29 +0530

    self affirmations sustain me at this point

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import pandas as pd
 import streamlit as st
 import random
 
+# Load playlist data
 playlist_data = pd.read_csv('playlist_data.csv')
 
-st.title('Decades Playlist Generator')
-st.markdown("""Select two decades from 1920 to 2010, and we'll generate a playlist of 10 random songs from each decade!""")
+# Set page title
+st.set_page_config(page_title='Modern Love: In Writing', page_icon=':heart:')
 
+# Page heading and synopsis
+st.title('Modern Love: In Writing')
+st.markdown('''
+The idea of modern love has evolved over the years, and popular music has played a significant role in shaping our understanding of what love means in different eras. From the romantic ballads of the 1950s to the more complex and nuanced portrayals of love in recent times, pop songs have reflected changing social norms, cultural attitudes, and personal experiences.
+
+With this tool, you can explore the evolution of modern love by generating a playlist of the top 10 pop songs from two different decades. Simply select your preferred decades from the dropdown menus and click the "Generate Playlist" button to get started!
+''')
+
+# Decades selector
 decades_dict = {'1920s': 1920,
                 '1930s': 1930,
                 '1940s': 1940,
@@ -21,6 +31,11 @@ decades = list(decades_dict.keys())
 
 decade1 = st.selectbox('Select the first decade:', decades)
 decade2 = st.selectbox('Select the second decade:', decades)
+
+start_year1 = decades_dict[decade1]
+end_year1 = start_year1 + 10
+start_year2 = decades_dict[decade2]
+end_year2 = start_year2 + 10
 
 if st.button('Generate Playlist'):
     playlist1 = playlist_data[(playlist_data['decade'] == decade1)]
