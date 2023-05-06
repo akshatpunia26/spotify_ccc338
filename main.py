a66b142c902869bc3ad134217f176a5c082aa574
Akshat Punia <92669600+akshatpunia26@users.noreply.github.com>
2023-05-06 07:59:43 +0530

    want to smash my face in the wall pain.gif

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import streamlit as st
-import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# Set up authentication manager
-CLIENT_ID = 'dc8611201d2a4d68ac59e3623d309096'
-CLIENT_SECRET = '470122036a274706a4f705ab88867fed'
-REDIRECT_URI = 'https://modern-love-spotify.streamlit.app/'
-SCOPE = 'playlist-modify-private,playlist-modify-public'
-CACHE_PATH = '.spotipyoauthcache'
+# Define the available decades
+DECADES = {
+    "1920s": "1920-1929",
+    "1930s": "1930-1939",
+    "1940s": "1940-1949",
+    "1950s": "1950-1959",
+    "1960s": "1960-1969",
+    "1970s": "1970-1979",
+    "1980s": "1980-1989",
+    "1990s": "1990-1999",
+    "2000s": "2000-2009",
+    "2010s": "2010-2019",
+}
 
+# Set up authentication manager
 auth_manager = SpotifyOAuth(
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    redirect_uri=REDIRECT_URI,
-    scope=SCOPE,
-    cache_path=CACHE_PATH,
+    client_id='dc8611201d2a4d68ac59e3623d309096',
+    client_secret='470122036a274706a4f705ab88867fed',
+    redirect_uri='https://modern-love-spotify.streamlit.app/',
+    scope='playlist-modify-private,playlist-modify-public',
+    cache_path='.spotipyoauthcache'
 )
 
 # Get access token
@@ -42,62 +49,26 @@ if not access_token:
 spotify = spotipy.Spotify(auth=access_token)
 user_dict = spotify.current_user()
 
-# Define the available decades
-DECADES = {
-    "1920s": "1920-1929",
-    "1930s": "1930-1939",
-    "1940s": "1940-1949",
-    "1950s": "1950-1959",
-    "1960s": "1960-1969",
-    "1970s": "1970-1979",
-    "1980s": "1980-1989",
-    "1990s": "1990-1999",
-    "2000s": "2000-2009",
-    "2010s": "2010-2019",
-}
-
-# Initialize the Spotipy client with the OAuth flow
-sp_oauth = SpotifyOAuth(
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    redirect_uri=REDIRECT_URI,
-    scope="playlist-modify-private,playlist-modify-public",
-    cache_path=".spotifycache",
-)
-
-# Get the user's access token
-access_token = None
-token_info = sp_oauth.get_cached_token()
-if token_info and sp_oauth.is_token_valid(token_info):
-    access_token = token_info["access_token"]
-if not access_token:
-    auth_url = sp_oauth.get_authorize_url()
-    st.write("Please visit this URL to authorize the application:", auth_url)
-    response = st.experimental_get_query_params()
-    code = response["code"]
-    token_info = sp_oauth.get_access_token(code)
-    access_token = token_info["access_token"]
-
-# Initialize the Spotipy client with the user's access token
-sp = spotipy.Spotify(auth=access_token)
-
 # Create the playlist
 playlist_name = st.text_input("Enter a name for the playlist:")
 description = st.text_input("Enter a description for the playlist:")
-playlist = sp.user_playlist_create(user=sp.current_user()["id"], name=playlist_name, public=False, description=description)
+playlist = spotify.user_playlist_create(user_dict['id'], name=playlist_name, public=False, description=description)
+
+# Get the two decades from the user
+decade1 = st.selectbox("Select the first decade:", options=list(DECADES.keys()))
+decade2 = st.selectbox("Select the second decade:", options=list(DECADES.keys()))
 
 # Initialize an empty list to hold the track URIs
 track_uris = []
 
-# Iterate over the decades and search for tracks
-for decade, year_range in DECADES.items():
+# Search for tracks from the selected decades and add them to the list
+for decade in [decade1, decade2]:
     st.write(f"Searching for {decade} songs...")
-    results = sp.search(q=f"year:{year_range}", type="track", limit=10)
+    results = spotify.search(q=f"year:{DECADES[decade]}", type="track", limit=10)
     for track in results["tracks"]["items"]:
         track_uris.append(track["uri"])
         st.write(f"{track['name']} by {track['artists'][0]['name']} ({decade}) - {track['external_urls']['spotify']}")
 
 # Add the tracks to the playlist
-sp.playlist_add_items(playlist["id"], track_uris)
+spotify.playlist_add_items(playlist["id"], track_uris)
 st.write(f"Playlist '{playlist_name}' created with {len(track_uris)} songs.")
-
